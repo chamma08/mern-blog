f6dd7d8303909c21fbff2fff1866322f71a9dca0
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2024-06-21 22:05:06 +0530

    Add redux persist

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -29,4 +29,4 @@ const userSlice = createSlice({
 
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
 
-export const userReducer = userSlice.reducer;
+export default userSlice.reducer;
